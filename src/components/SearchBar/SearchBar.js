a79bf626dbd0efe93431acceacd1be577b519174
Howard Do <howarddo2208@gmail.com>
2022-06-18 20:21:16 +0700

    zustand for search

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,36 +1,38 @@
-import React, { useState } from "react";
-import useSearch from "../../hooks/useSearch.js";
+import React, { useState } from 'react'
+import { useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
+import { useSearch } from '../../stateManagements'
 
 const SearchBar = (props) => {
-  const { search } = useSearch();
-  const [value, setValue] = useState("");
-
+  const { search, searchValue } = useSearch()
+  const [value, setValue] = useState('')
+  const history = useHistory()
   //handle input change for search bar
   const onInputChange = (evt) => {
-    setValue(evt.target.value);
-  };
+    setValue(evt.target.value)
+  }
 
   //handle 'enter' press event
   const onEnterKey = (evt) => {
     if (evt.keyCode === 13) {
-      evt.preventDefault();
-      search(value);
-      setValue("");
+      evt.preventDefault()
+      search(value, history)
     }
-  };
+  }
 
   return (
     <input
       className={
-        props.showSearchOnMobile ? "search-bar--mobile " : "search-bar "
+        props.showSearchOnMobile ? 'search-bar--mobile ' : 'search-bar '
       }
       key="random1"
       value={value}
-      placeholder={"Search..."}
+      placeholder={'Search...'}
       onChange={onInputChange}
       onKeyDown={onEnterKey}
     />
-  );
-};
+  )
+}
 
-export default SearchBar;
+export default SearchBar
+
