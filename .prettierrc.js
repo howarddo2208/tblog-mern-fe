f956d4a8921b4cd91c06028068eb91aa64a7156a
Howard Do <howarddo2208@gmail.com>
2022-06-27 02:09:05 +0700

    use trunk

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+// trunk-ignore(eslint)
+export default {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+}
--- src/pages/NewPost/NewPost.js
+++ src/pages/NewPost/NewPost.js
@@ -7,62 +7,64 @@ import { useAuth } from '../../stateManagements'
 import { TextField } from '@mui/material'
 
 const NewPost = () => {
-  const auth = useAuth()
-  const history = useHistory()
-  const { currentUser } = auth
-  const { isLoading, sendReq, error, clearError } = useHttpClient()
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm()
+    const auth = useAuth()
+    const history = useHistory()
+    const { currentUser } = auth
+    const { isLoading, sendReq, error, clearError } = useHttpClient()
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm()
 
-  const onSubmit = (data) => console.log(data)
+    const onSubmit = (data) => console.log(data)
 
-  const [submitting, setSubmitting] = useState(false)
+    const [submitting, setSubmitting] = useState(false)
 
-  // const postSubmitHandle = async (evt) => {
-  //   setSubmitting(true)
-  //   evt.preventDefault()
-  //   console.log('value')
-  //   // const formData = appendData(formValues)
-  //   // formData.append('author', currentUser.userId)
+    // const postSubmitHandle = async (evt) => {
+    //   setSubmitting(true)
+    //   evt.preventDefault()
+    //   console.log('value')
+    //   // const formData = appendData(formValues)
+    //   // formData.append('author', currentUser.userId)
 
-  //   // try {
-  //   //   await sendReq(
-  //   //     `${process.env.REACT_APP_BASE_URL}/posts`,
-  //   //     'POST',
-  //   //     formData,
-  //   //     {
-  //   //       Authorization: `Bearer ${currentUser.token}`,
-  //   //     }
-  //   //   )
-  //   //   history.push('/')
-  //   // } catch (err) {
-  //   //   console.log('err', err)
-  //   // } finally {
-  //   //   setSubmitting(false)
-  //   // }
-  // }
+    //   // try {
+    //   //   await sendReq(
+    //   //     `${process.env.REACT_APP_BASE_URL}/posts`,
+    //   //     'POST',
+    //   //     formData,
+    //   //     {
+    //   //       Authorization: `Bearer ${currentUser.token}`,
+    //   //     }
+    //   //   )
+    //   //   history.push('/')
+    //   // } catch (err) {
+    //   //   console.log('err', err)
+    //   // } finally {
+    //   //   setSubmitting(false)
+    //   // }
+    // }
 
-  return (
-    <>
-      <ErrorModal error={error} onClose={clearError} />
-      {!isLoading && (
+    return (
         <>
-          <form onSubmit={handleSubmit()} className="form form__create">
-            <TextField
-              id="outlined-basic"
-              label="Outlined"
-              variant="outlined"
-            />
-          </form>
+            <ErrorModal error={error} onClose={clearError} />
+            {!isLoading && (
+                <>
+                    <form
+                        onSubmit={handleSubmit()}
+                        className="form form__create"
+                    >
+                        <TextField
+                            id="outlined-basic"
+                            label="Outlined"
+                            variant="outlined"
+                        />
+                    </form>
+                </>
+            )}
         </>
-      )}
-    </>
-  )
+    )
 }
 
 export default NewPost
-
